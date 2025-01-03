46a777c2f5b7c74230c0ef779d1d8ea83eb4c5ae
djumanov <djumanovdev@gmail.com>
2025-01-03 11:07:56 +0500

    currency api

---
+++ main.py
@@ -0,0 +1,201 @@
+kurs = [
+    {
+        "title": "BAA dirhami",
+        "code": "AED",
+        "cb_price": "3517.79",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Avstraliya dollari",
+        "code": "AUD",
+        "cb_price": "8044.29",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Kanada dollari",
+        "code": "CAD",
+        "cb_price": "8975.67",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Shveytsariya franki",
+        "code": "CHF",
+        "cb_price": "14362.47",
+        "nbu_buy_price": "14150.00",
+        "nbu_cell_price": "14850.00",
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Xitoy yuani",
+        "code": "CNY",
+        "cb_price": "1770.12",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Daniya kronasi",
+        "code": "DKK",
+        "cb_price": "1801.06",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Misr funti",
+        "code": "EGP",
+        "cb_price": "253.94",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Yevro",
+        "code": "EUR",
+        "cb_price": "13436.01",
+        "nbu_buy_price": "13310.00",
+        "nbu_cell_price": "13480.00",
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Angliya funt sterlingi",
+        "code": "GBP",
+        "cb_price": "16190.65",
+        "nbu_buy_price": "15950.00",
+        "nbu_cell_price": "16650.00",
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Islandiya kronasi",
+        "code": "ISK",
+        "cb_price": "92.60",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Yaponiya iyenasi",
+        "code": "JPY",
+        "cb_price": "82.07",
+        "nbu_buy_price": "70.00",
+        "nbu_cell_price": "85.00",
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Koreya respublikasi voni",
+        "code": "KRW",
+        "cb_price": "8.81",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Quvayt dinori",
+        "code": "KWD",
+        "cb_price": "41922.39",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Qozog‘iston tengesi",
+        "code": "KZT",
+        "cb_price": "25.03",
+        "nbu_buy_price": "15.00",
+        "nbu_cell_price": "25.00",
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Livan funti",
+        "code": "LBP",
+        "cb_price": "0.14",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Malayziya ringgiti",
+        "code": "MYR",
+        "cb_price": "2891.13",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Norvegiya kronasi",
+        "code": "NOK",
+        "cb_price": "1134.80",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Polsha zlotiysi",
+        "code": "PLN",
+        "cb_price": "3149.88",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Rossiya rubli",
+        "code": "RUB",
+        "cb_price": "130.17",
+        "nbu_buy_price": "110.00",
+        "nbu_cell_price": "135.00",
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Shvetsiya kronasi",
+        "code": "SEK",
+        "cb_price": "1167.09",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Singapur dollari",
+        "code": "SGD",
+        "cb_price": "9509.44",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Turkiya lirasi",
+        "code": "TRY",
+        "cb_price": "366.34",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "Ukraina grivnasi",
+        "code": "UAH",
+        "cb_price": "308.92",
+        "nbu_buy_price": None,
+        "nbu_cell_price": None,
+        "date": "02.01.2025 18:00:01"
+    },
+    {
+        "title": "AQSh dollari",
+        "code": "USD",
+        "cb_price": "12920.48",
+        "nbu_buy_price": "12850.00",
+        "nbu_cell_price": "12930.00",
+        "date": "02.01.2025 18:00:01"
+    }
+]
+
+
+money = float(input("money >> "))
+for currency in kurs:
+    if currency['nbu_buy_price'] is not None:
+        result = money / float(currency['nbu_buy_price'])
+        print(currency['code'], result)
